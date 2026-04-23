bb5cd8a42eb5ab5d0bd662217923edabf702bec0
Shawã Maheshananda <shawa.maheshananda@ibiete.org>
2026-04-22 22:40:52 -0300

    fix: remove rede web e simplifica template

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 set -e
-
-echo "===> Iniciando deploy"
-
 echo "===> Atualizando código"
-git pull origin main
-
-echo "===> Subindo containers"
-docker compose up -d --build
-
-echo "===> Limpando imagens antigas"
-docker image prune -f
-
-echo "===> Deploy finalizado com sucesso"
+git fetch origin
+git reset --hard origin/main
+echo "===> Deploy finalizado"
